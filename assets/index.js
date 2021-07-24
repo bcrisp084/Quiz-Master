f2428af4be1a89e7360c634f9ae81b57dbb0c79b
Brian Crisp <bcrisp084@yahoo.com>
2021-07-23 22:38:47 -0400

    made some layout changes, still working on the overall design

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,56 @@
-const startBtn = document.querySelector('.start')
-const questionContainer = document.querySelector('#questionContainer')
+const startBtn = document.querySelector('#start')
+const questionContainer = document.querySelector('#iniatialContainer')
 
 
-startBtn.addEventListener('click', startGame())
+startBtn.addEventListener("click", startGame)
 
 function startGame() {
-    startBtn.classList.add('hide')
-    questionContainer.classList.add('hide')
-    console.log(startBtn)
-    
+    startBtn.classList.add('hide');
+
 }
+console.log(startBtn)
+
+
+const quizQuestion = [
+    {
+        question: 'How many licks does it take to get to the center of a tootsie pop',
+        answers: [],
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answers: [],
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answers: [],
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answers: [],
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answers: [],
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answers: [],
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answers: [],
+        correctAnswer: '',
+    },
+    {
+        question: '',
+        answers: [],
+        correctAnswer: '',
+    },
+]
+
