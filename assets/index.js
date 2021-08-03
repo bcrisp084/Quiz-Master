e3e0b29e86cd6be6aab75635c08ddd10e81a4653
Brian Crisp <bcrisp084@yahoo.com>
2021-08-02 20:35:30 -0400

    created the function to load the question along with the answers. also add validation for the timeer

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,7 @@ const startContainer = document.querySelector('.start-container')
 const timer = document.querySelector(".timer")
 let secondsLeft = 60;
 let timeInterval;
+let currentIndex;
 
 
 startBtn.addEventListener("click", startGame)
@@ -11,8 +12,10 @@ startBtn.addEventListener("click", startGame)
 function startGame() {
     startBtn.classList.add('hide');
     startContainer.classList.add('hide')
-    questionContainer.classList.remove('hide')
-    startTimer()
+    questionContainer.classList.remove('hide');
+    currentIndex = 0;
+    startTimer();
+    loadQuestions();
 }
 
 function startTimer() {
@@ -26,14 +29,39 @@ function startTimer() {
 }
 
 function loadQuestions() {
+    document.querySelector(".question").textContent = quizQuestion[currentIndex].question;
+    document.querySelector("#choiceA").textContent = quizQuestion[currentIndex].answers[0];
+    document.querySelector("#choiceB").textContent = quizQuestion[currentIndex].answers[1];
+    document.querySelector("#choiceC").textContent = quizQuestion[currentIndex].answers[2];
+    document.querySelector("#choiceD").textContent = quizQuestion[currentIndex].answers[3];
 
+}
+
+var answerBtns = document.querySelectorAll(".btn")
+for (var i = 0; i < answerBtns.length; i++) {
+    answerBtns[i].addEventListener("click", checkAnswer);
+}
+//Validating answers and checks quiz progress
+function checkAnswer() {
+    if (this.textContent === quizQuestion[currentIndex].answers) {
+        secondsLeft += 15;
+    }
+    else {
+        secondsLeft -= 15;
+    }
+    currentIndex++;
+    if (currentIndex === quizQuestion.length) {
+        questionContainer.style.display = 'none';
+        enterInitials.style.display = "block";
+    }
+    loadQuestions();
 }
 
 const quizQuestion = [
     {
         question: 'How many licks does it take to get to the center of a tootsie pop',
-        answers: [],
-        correctAnswer: '',
+        answers: ["10", "14", "99", "477"],
+        correctAnswer: '477',
     },
     {
         question: '',
