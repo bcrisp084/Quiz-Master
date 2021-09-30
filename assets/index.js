236c65b43f67ac6777c2a31ad96ec5b8a113b4e2
Brian Crisp <bcrisp084@yahoo.com>
2021-09-29 20:46:16 -0400

    changed the main card layout

--- assets/index.js
+++ assets/index.js
@@ -21,7 +21,7 @@ function startGame() {
     timer.classList.remove('hide')
     currentIndex = 0;
     startTimer();
-    loadQuestions();
+    loadEasyQuestions();
 }
 
 function startTimer() {
@@ -35,26 +35,13 @@ function startTimer() {
     }, 1000);
 }
 
-function loadQuestions() {
-    document.querySelector(".question").textContent = quizQuestion[currentIndex]?.question;
-    document.querySelector("#choiceA").textContent = quizQuestion[currentIndex]?.answers[0];
-    document.querySelector("#choiceB").textContent = quizQuestion[currentIndex]?.answers[1];
-    document.querySelector("#choiceC").textContent = quizQuestion[currentIndex]?.answers[2];
-    document.querySelector("#choiceD").textContent = quizQuestion[currentIndex]?.answers[3];
-    var answerBtns = document.querySelectorAll(".btn")
-    for (var i = 0; i < answerBtns.length; i++) {
-        answerBtns[i].addEventListener("click", loadQuestions);
-    }
-    currentIndex++;
-    gameOver()
+function loadEasyQuestions() {
+    const currentQuestion = easyQuestions[currentIndex].question;
+    const questionEl = document.querySelector('.question')
+    questionEl.textContent = currentQuestion
+
 }
 
-var btnGrid = document.querySelector(".btn-grid")
-btnGrid.addEventListener("click", function (event) {
-    console.log(event)
-    console.log(event.target)
-    console.log(event.target.id)
-})
 
 
 
@@ -75,7 +62,7 @@ function gameOver() {
     }
 }
 
-const quizQuestion = [
+const easyQuestions = [
     {
         question: "What geometric shape is used in MMA sports?",
         answers: ["pentagon", "hexagon", "square", "octagon"],
@@ -117,4 +104,48 @@ const quizQuestion = [
         correctAnswer: "A promise",
     },
 ]
+
+const hardQuestions = [
+    {
+        question: "What geometric shape is used in MMA sports?",
+        answers: ["pentagon", "hexagon", "square", "octagon"],
+        correctAnswer: "octagon",
+    },
+    {
+        question: "what is Cynophobia?",
+        answers: ["fear of heights", "fear of salt", "fear of dogs", "fear of cars"],
+        correctAnswer: "fear of dogs",
+    },
+    {
+        question: "Which animal can be seen of a Porsche logo?",
+        answers: ["lion", "Elephant", "Horse", "Cheetah"],
+        correctAnswer: "Horse",
+    },
+    {
+        question: "What is the name of Batman's sidekick?",
+        answers: ["Robin", "Reggie", "Larry", "Oscar"],
+        correctAnswer: "Robin",
+    },
+    {
+        question: "What was the first soft Drink in space?",
+        answers: ["Pepsi", "Mountain Dew", "Coca Cola", "Sprite"],
+        correctAnswer: "Coca Cola",
+    },
+    {
+        question: "Which is the only edible food that never goes bad?",
+        answers: ["Jello", "Honey", "Twinkies", "Marshmallows"],
+        correctAnswer: "Honey",
+    },
+    {
+        question: "What was the first toy to be advertised on TV?",
+        answers: ["GI Joe", "Cabbage patch kids", "Alf", "Mr Potato Head"],
+        correctAnswer: "Mr Potato Head",
+    },
+    {
+        question: "What can be broken but is never held?",
+        answers: ["A heart", "A promise", "A dance", "Glass"],
+        correctAnswer: "A promise",
+    },
+]
+
 
