498661377fad645d0eb83f0308f430f577a622cc
Brian Crisp <bcrisp084@yahoo.com>
2021-09-29 23:10:27 -0400

    updated some function for the game to have a hard set of questions or an easy set of questions

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,8 @@ const startContainer = document.querySelector('.start-container')
 const enterInitials = document.querySelector('.enterInitials')
 const timer = document.querySelector(".timer")
 const header = document.querySelector('.header')
+const questionEl = document.querySelector('.question')
+const insight = document.querySelector('.insight')
 let secondsLeft = 60;
 let timeInterval;
 let currentIndex;
@@ -12,11 +14,10 @@ let score;
 
 
 
-easyBtn.addEventListener("click", startGame)
-hardBtn.addEventListener('click', startGame)
+easyBtn.addEventListener("click", easyGame)
+hardBtn.addEventListener('click', hardGame)
 
-function startGame() {
-    startBtn.classList.add('hide');
+function easyGame() {
     startContainer.classList.add('hide')
     questionContainer.classList.remove('hide');
     header.classList.add('hide')
@@ -24,6 +25,16 @@ function startGame() {
     currentIndex = 0;
     startTimer();
     loadEasyQuestions();
+}
+
+function hardGame() {
+    startContainer.classList.add('hide')
+    questionContainer.classList.remove('hide');
+    header.classList.add('hide')
+    timer.classList.remove('hide')
+    currentIndex = 0;
+    startTimer();
+    loadHardQuestions()
 }
 
 function startTimer() {
@@ -37,12 +48,80 @@ function startTimer() {
     }, 1000);
 }
 
-function loadEasyQuestions() {
+function loadEasyQuestions(current) {
     const currentQuestion = easyQuestions[currentIndex].question;
     const questionEl = document.querySelector('.question')
-    questionEl.textContent = currentQuestion
+    questionEl.innerHTML = currentQuestion
+    for (const answer of easyQuestions[currentIndex].answers) {
+        const button = document.createElement('button')
+        button.setAttribute('data-answer', answer)
+        button.setAttribute('id', 'answer-button')
+        button.innerHTML = answer
+        const btnGrid = document.querySelector('.btn-grid')
+        button.addEventListener('click', function (event) {
+            const correct = easyQuestions[currentIndex].correctAnswer
+            const chosen = button.getAttribute('data-answer')
+            console.log('corrrect', correct)
+            console.log('chosen', chosen)
+            if (correct === chosen) {
+                insight.innerHTML = ''
+                btnGrid.innerHTML = ''
+                questionEl.innerHTML = ''
+                const correctInfo = document.createElement('h1')
+                correctInfo.textContent = 'Correct'
+                correctInfo.setAttribute('class', 'correctInfo')
+                insight.appendChild(correctInfo)
+                currentIndex++
+                loadEasyQuestions(currentIndex)
 
+            } else {
+                insight.innerHTML = ''
+                btnGrid.innerHTML = ''
+                questionEl.innerHTML = ''
+                const incorrectInfo = document.createElement('h1')
+                incorrectInfo.textContent = 'Incorrect'
+                incorrectInfo.setAttribute('class', 'incorrectInfo')
+                insight.appendChild(incorrectInfo)
+                currentIndex++
+                loadEasyQuestions(currentIndex)
+            }
+        })
+        btnGrid.appendChild(button)
+    }
 }
+
+function loadHardQuestions(current) {
+    const currentQuestion = hardQuestions[currentIndex].question;
+    const questionEl = document.querySelector('.question')
+    questionEl.innerHTML = currentQuestion
+    for (const answer of hardQuestions[currentIndex].answers) {
+        const button = document.createElement('button')
+        button.setAttribute('data-answer', answer)
+        button.setAttribute('id', 'answer-button')
+        button.innerHTML = answer
+        const btnGrid = document.querySelector('.btn-grid')
+        button.addEventListener('click', function (event) {
+            const correct = hardQuestions[currentIndex].correctAnswer
+            const chosen = button.getAttribute('data-answer')
+            console.log('corrrect', correct)
+            console.log('chosen', chosen)
+            if (correct === chosen) {
+                btnGrid.innerHTML = ''
+                questionEl.innerHTML = ''
+                currentIndex++
+                loadHardQuestions(currentIndex)
+            } else {
+                btnGrid.innerHTML = ''
+                questionEl.innerHTML = ''
+                currentIndex++
+                loadHardQuestions(currentIndex)
+            }
+        })
+        btnGrid.appendChild(button)
+    }
+}
+
+
 
 
 
@@ -109,44 +188,44 @@ const easyQuestions = [
 
 const hardQuestions = [
     {
-        question: "What geometric shape is used in MMA sports?",
-        answers: ["pentagon", "hexagon", "square", "octagon"],
-        correctAnswer: "octagon",
+        question: "What musical term indicates a chord where the notes are played one after another rather than all together?",
+        answers: ["A Capella", "Atonal", "Arpeggio", "Baritone"],
+        correctAnswer: "Arpeggio",
     },
     {
-        question: "what is Cynophobia?",
-        answers: ["fear of heights", "fear of salt", "fear of dogs", "fear of cars"],
-        correctAnswer: "fear of dogs",
+        question: "What color does gold leaf appear if you hold it up to the light?",
+        answers: ["Green", "Bronze", "Silver", "Red"],
+        correctAnswer: "Green",
     },
     {
-        question: "Which animal can be seen of a Porsche logo?",
-        answers: ["lion", "Elephant", "Horse", "Cheetah"],
-        correctAnswer: "Horse",
+        question: "Who was the first person to suggest Daylight Savings Times?",
+        answers: ["Thomas Edison", "Benjamin Franklin", "James Madison", "Alexander Hamilton"],
+        correctAnswer: "Benjamin Franklin",
     },
     {
-        question: "What is the name of Batman's sidekick?",
-        answers: ["Robin", "Reggie", "Larry", "Oscar"],
-        correctAnswer: "Robin",
+        question: "Emetophobia is the fear of?",
+        answers: ["Fear of Feces", "Fear of Mice", "Fear of People", " Fear of Vomit"],
+        correctAnswer: " Fear of Vomit",
     },
     {
-        question: "What was the first soft Drink in space?",
-        answers: ["Pepsi", "Mountain Dew", "Coca Cola", "Sprite"],
-        correctAnswer: "Coca Cola",
+        question: "What famous actress once tried to hire a hitman to kill her?",
+        answers: ["Angelina Jolie", "Demi Moore", "Cameron Diaz", "Sally Field"],
+        correctAnswer: "Angelina Jolie",
     },
     {
-        question: "Which is the only edible food that never goes bad?",
-        answers: ["Jello", "Honey", "Twinkies", "Marshmallows"],
-        correctAnswer: "Honey",
+        question: "What is the only king in a deck of cards without a mustache??",
+        answers: ["King of clubs", "King of hearts", "King of spades", "King of diamonds"],
+        correctAnswer: "King of hearts",
     },
     {
-        question: "What was the first toy to be advertised on TV?",
-        answers: ["GI Joe", "Cabbage patch kids", "Alf", "Mr Potato Head"],
-        correctAnswer: "Mr Potato Head",
+        question: "What is the little dot above a lowercase “i” or “j” called?",
+        answers: ["Tilda", "Dot", "Tittle", "Tempe"],
+        correctAnswer: "Tittle",
     },
     {
-        question: "What can be broken but is never held?",
-        answers: ["A heart", "A promise", "A dance", "Glass"],
-        correctAnswer: "A promise",
+        question: "What country has competitive office chair racing?",
+        answers: ["United States", "Russia", "Turkey", "Japan"],
+        correctAnswer: "Japan",
     },
 ]
 
